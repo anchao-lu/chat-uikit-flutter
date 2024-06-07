d047c0ba997b359d465fe5a1d9fea2bd121e6844
huangsir0 <47199570+huangsir0@users.noreply.github.com>
2024-06-07 16:28:27 +0800

    聊天框添加发送按钮

--- lib/ui/views/TIMUIKitChat/TIMUIKitTextField/tim_uikit_text_field_layout/wide.dart
+++ lib/ui/views/TIMUIKitChat/TIMUIKitTextField/tim_uikit_text_field_layout/wide.dart
@@ -1121,6 +1121,23 @@ class _TIMUIKitTextFieldLayoutWideState
                   ],
                 ),
               ),
+              if (PlatformUtils().isDesktop)
+                Container(
+                  color: hexToColor("fafafa"),
+                  padding:
+                  const EdgeInsets.only(top: 10, bottom: 10, right: 20),
+                  child: Align(
+                    alignment: Alignment.bottomRight,
+                    child: ElevatedButton(
+                        onPressed: () {
+                          widget.onSubmitted();
+                        },
+                        child: Container(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 5, vertical: 3),
+                            child: Text(TIM_t("发送")))),
+                  ),
+                )
             ],
           ),
         ));
