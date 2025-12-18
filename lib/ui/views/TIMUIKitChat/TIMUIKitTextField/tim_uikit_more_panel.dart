bc9df17d91925d061ca7c4975e25998c20ecca3a
lanch <lanch992@gmail.com>
2025-12-18 11:24:58 +0800

    【还原】iOS 拍照分辨率

--- lib/ui/views/TIMUIKitChat/TIMUIKitTextField/tim_uikit_more_panel.dart
+++ lib/ui/views/TIMUIKitChat/TIMUIKitTextField/tim_uikit_more_panel.dart
@@ -704,7 +704,7 @@ class _MorePanelState extends TIMUIKitState<MorePanel> {
         return ResolutionPreset.ultraHigh;
       }
     } else {
-      return ResolutionPreset.max;
+      return ResolutionPreset.veryHigh;
     }
   }
 
